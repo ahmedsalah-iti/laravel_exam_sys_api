8203600f434e98f2d9d92d908e23ab331ea100ee
Ahmed Salah <ahmedsalah.iti@gmail.com>
2025-06-10 01:16:21 +0300

    fininsh ExamAttempt  model

--- app/Models/ExamAttempt.php
+++ app/Models/ExamAttempt.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class ExamAttempt extends Model
 {
-    //
+    protected $fillable = ['user_id', 'exam_id', 'score', 'started_at', 'submitted_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function exam()
+    {
+        return $this->belongsTo(Exam::class);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(StudentAnswer::class);
+    }
 }
