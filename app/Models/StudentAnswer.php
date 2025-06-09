d17e32e4ff01f6b9bc3d236c92a0bec68ee069fd
Ahmed Salah <ahmedsalah.iti@gmail.com>
2025-06-10 01:17:13 +0300

    fininsh StudentAnswer  model

--- app/Models/StudentAnswer.php
+++ app/Models/StudentAnswer.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudentAnswer extends Model
 {
-    //
+    protected $fillable = ['exam_attempt_id', 'question_id', 'choice_id'];
+
+    public function examAttempt()
+    {
+        return $this->belongsTo(ExamAttempt::class);
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function choice()
+    {
+        return $this->belongsTo(Choice::class);
+    }
 }
