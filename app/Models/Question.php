0df9f6273e47be2ed0dd5fd9cd621875e5463fb8
Ahmed Salah <ahmedsalah.iti@gmail.com>
2025-06-10 01:14:30 +0300

    fininsh Question  model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $fillable = ['exam_id', 'question_text'];
+    public function exam(){
+        return $this->belongsTo(Exam::class);
+    }
+    public function choices(){
+        return $this->hasMany(Choice::class);
+    }
+    public function studentAnswers()
+    {
+        return $this->hasMany(StudentAnswer::class);
+    }
 }
