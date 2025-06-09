715f10b3608199c7f18016085447a598cdba5609
Ahmed Salah <ahmedsalah.iti@gmail.com>
2025-06-10 01:50:55 +0300

    fininsh ExamController

---
+++ app/Http/Controllers/Api/ExamController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Exam;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class ExamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $exams = Auth::user()->role === 'student'
+            ? Exam::all()
+            : Exam::where('created_by', Auth::id())->get();
+        return response()->json($exams);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|min:5|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $exam = Exam::create([
+            'title' => $validated['title'],
+            'description' => $validated['description'],
+            'created_by' => Auth::id(),
+        ]);
+
+        return response()->json($exam, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $exam = Exam::findOrFail($id);
+        if(!$exam){
+            return response()->json(['msg'=>'exam not found'],404);
+        }
+        return response()->json($exam->load('questions.choices'),200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $exam = Exam::findOrFail($id);
+        if(!$exam){
+            return response()->json(['msg'=>'exam not found'],404);
+        }
+        if ($exam->creator() != Auth::id()){
+            return response()->json(['msg'=>'you dont have access to this exam'],403);
+        }
+        $validated = $request->validate([
+            'title' => 'required|string|min:5|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $exam->update($validated);
+        return response()->json($exam);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $exam = Exam::findOrFail($id);
+        if(!$exam){
+            return response()->json(['msg'=>'exam not found'],404);
+        }
+        if ($exam->creator() != Auth::id()){
+            return response()->json(['msg'=>'you dont have access to this exam'],403);
+        }
+        $exam->delete();
+        return response()->json(['message' => 'Exam deleted']);
+    }
+}
