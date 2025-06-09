a850e5d47a8e41d59a6f959b3353de946ba5b1a1
Ahmed Salah <ahmedsalah.iti@gmail.com>
2025-06-10 01:15:26 +0300

    fininsh Choice  model

--- app/Models/Choice.php
+++ app/Models/Choice.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Choice extends Model
 {
-    //
+    protected $fillable = ['question_id', 'choice_text', 'is_correct'];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function studentAnswers()
+    {
+        return $this->hasMany(StudentAnswer::class);
+    }
 }
