f64174c5c710842dc84ed0e9dada2e74dcb5b539
Ahmed Salah <ahmedsalah.iti@gmail.com>
2025-06-10 01:12:11 +0300

    fininsh exam  model

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exam extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'created_by'];
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+        public function examAttempts()
+    {
+        return $this->hasMany(ExamAttempt::class);
+    }
 }
